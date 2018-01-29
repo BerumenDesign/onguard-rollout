f56ea2b7d165a2350925ddaa22ed3c89c16884e3
Mohammed Qureshi <mohammed.qureshi@gmail.com>
2018-01-28 19:51:09 -0700

    - handlers for onChange event, pass data to parent.

--- src/components/formAdmin.js
+++ src/components/formAdmin.js
@@ -1,32 +1,36 @@
 import React from 'react';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
-import SelectField from 'material-ui/SelectField';
-import MenuItem from 'material-ui/MenuItem';
 import FlatButton from 'material-ui/FlatButton';
+import RegionSelector from './common/RegionSelector';
 
-
-
-const TextForm = () => (<div class="container">
-  <form>
-    <h2>Data Center Region</h2>
-    <h3>All fields are required</h3>
-    <div className="formRow">
-      <div className="formColumn">
-        <SelectField autoWidth={true} floatingLabelText="Select region">
-          <MenuItem value={1} primaryText="Select list for region"/>
-        </SelectField >
-        <br/>
-        <TextField floatingLabelText="Estimated number of employees" floatingLabelFixed={false}/>
-        <br/>
-
-      </div>
-    </div>
-    <div className="formButton">
-      <FlatButton label="Cancel"/>
-      <RaisedButton label="Continue" primary={true}/>
-    </div>
-  </form>
-</div>);
+class TextForm extends React.Component {
+  onRegionChange(region) {
+    this.props.onChange({region});
+  }
+  onEmployeeCountChange(e) {
+    this.props.onChange({[e.target.name]: e.target.value});
+  }
+  render() {
+    return (
+      <form>
+        <h2>Data Center Region</h2>
+        <h3>All fields are required</h3>
+        <div className="formRow">
+          <div className="formColumn">
+            <RegionSelector onChange={this.onRegionChange.bind(this)} value={this.props.region} />
+            <br/>
+            <TextField name="employeeCount" floatingLabelText="Estimated number of employees" floatingLabelFixed={false} onChange={this.onEmployeeCountChange.bind(this)} value={this.props.employeeCount} />
+            <br/>
+          </div>
+        </div>
+        <div className="formButton">
+          <FlatButton label="Cancel"/>
+          <RaisedButton label="Continue" primary={true}/>
+        </div>
+      </form>
+    );
+  }
+};
 
 export default TextForm;
