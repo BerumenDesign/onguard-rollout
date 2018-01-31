81ff336e57984f1891d155655198d135ea724132
Mohammed Qureshi <mohammed.qureshi@gmail.com>
2018-01-30 22:04:41 -0700

    - show Plan information after IMEI is verified - notify parent on validation and set state to verified

--- src/components/common/PrepaidValidator.js
+++ src/components/common/PrepaidValidator.js
@@ -3,12 +3,14 @@ import RaisedButton from 'material-ui/RaisedButton';
 import Paper from 'material-ui/Paper';
 import Divider from 'material-ui/Divider';
 import Imei from './Imei';
+import Validation from '../../utils/validation';
 
 class PrepaidValidator extends React.Component {
     constructor() {
       super();
       this.state = {
-        imei: ''
+        imei: '',
+        verified: false
       };
       this.onChange = this.onChange.bind(this);
       this.verify = this.verify.bind(this);
@@ -17,7 +19,17 @@ class PrepaidValidator extends React.Component {
       this.setState({imei});
     }
     verify() {
-      //verify IMEI with some API call and return data to onValidated prop function
+        Validation.IMEI(this.state.imei).then(function() {
+            //verify IMEI with some API call and return data to onValidated prop function
+            this.props.onValidated({
+                plan: 'LITE PTT LW',
+                feats: ['24/7 ERC Monitoring', 'Indoor location / custom map overlays', 'Personal gas detector support'],
+                expire: 'January 25, 2019'
+            });
+            this.setState({verified: true});
+        }.bind(this)).catch(function() {
+            //handle invalid IMEI
+        });
     }
     render() {
       return (
@@ -25,23 +37,28 @@ class PrepaidValidator extends React.Component {
           <div className="formColumn">
             <h4>Use prepaid program</h4>
             <p>Enter 1 IMEI from the batch of phones purchased to lookup prepaid program eligibility</p>
-            <Imei name="imei" value={this.state.imei} onChange={this.onChange} />
+            <Imei name="imei" value={this.state.imei} onChange={this.onChange} disabled={this.state.verified} />
             <br/>
-            <RaisedButton label="Verify code" onClick={this.verify} />
+            {this.state.verified ? <div>VERIFIED CODE</div> : <RaisedButton label="Verify code" onClick={this.verify} />}
   
-            <Paper zDepth={1} className="activePlan">
-              <h4>Your plan is now active</h4>
-              <p>Active until January 25, 2018</p>
-              <Divider/>
-              <h5>Prepaid billing plan</h5>
-              <h4>LITE PTT LW</h4>
-              <p>This plan includes the following add ons</p>
-              <ul>
-                <li>24/7 ERC Monitoring</li>
-                <li>Indoor location / custom map overlays</li>
-                <li>Personal gas detector support</li>
-              </ul>
-            </Paper>
+            {
+                this.state.verified ?
+                    (
+                        <Paper zDepth={1} className="activePlan">
+                            <h4>Your plan is now active</h4>
+                            <p>Active until {this.props.billing.expire}</p>
+                            <Divider/>
+                            <h5>Prepaid billing plan</h5>
+                            <h4>{this.props.billing.plan}</h4>
+                            <p>This plan includes the following add ons</p>
+                            <ul>
+                                {
+                                    this.props.billing.feats.map((feat) => (<li>{feat}</li>))
+                                }
+                            </ul>
+                        </Paper>
+                    ) : null
+            }
           </div>
         </div>
       );
