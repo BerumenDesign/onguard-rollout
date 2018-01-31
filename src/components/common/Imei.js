9f2394430bae152438fbfba3cb873d911509cb4d
Mohammed Qureshi <mohammed.qureshi@gmail.com>
2018-01-30 22:03:55 -0700

    - handling for error and disabling of IMEI field, manage dirty state

--- src/components/common/Imei.js
+++ src/components/common/Imei.js
@@ -5,7 +5,7 @@ import TextField from 'material-ui/TextField';
 class Imei extends React.Component {
     constructor() {
       super();
-      this.state = {valid: true};
+      this.state = {valid: true, dirty: false};
       this.onChange = this.onChange.bind(this);
     }
     componentDidMount() {
@@ -14,18 +14,22 @@ class Imei extends React.Component {
       }.bind(this)).catch(function() {
         this.setState({ valid: false });
       }.bind(this));
-      
+    }
+    showError() {
+        if (this.state.dirty) {
+            return this.state.valid ? null : 'Invalid IMEI';
+        }
     }
     onChange(e) {
       this.props.onChange(e.target.value);
       Validation.IMEI(e.target.value).then(function() {
-        this.setState({ valid: true });
+        this.setState({ valid: true, dirty: true });
       }.bind(this)).catch(function() {
-        this.setState({ valid: false });
+        this.setState({ valid: false, dirty: true });
       }.bind(this));
     }
     render() {
-      return <TextField floatingLabelText="Enter IMEI" value={this.props.value} onChange={this.onChange} errorText={this.state.valid ? null : 'Invalid IMEI'} />;
+      return <TextField floatingLabelText="Enter IMEI" value={this.props.value} onChange={this.onChange} errorText={this.showError()} disabled={this.props.disabled} />;
     }
 };
 
