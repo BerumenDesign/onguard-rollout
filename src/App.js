eef7cbdde9efd9786c45b252f24ffdf4d51a30ee
Mohammed Qureshi <mohammed.qureshi@gmail.com>
2018-02-04 20:15:33 -0700

    - added validation handling for steps and forms - added additional forms to steps

--- src/App.js
+++ src/App.js
@@ -45,30 +45,72 @@ class Register extends React.Component {
         type: 'prepaid',
         plan: '',
         feats: [],
-        addons: []
+        addons: [],
+        payment: {
+          type: ''
+        }
+      },
+      validation: {
+        valid: false,
+        fields: {}
       }
     };
     this.onChange = this.onChange.bind(this);
     this.nextStep = this.nextStep.bind(this);
+    this.onValidation = this.onValidation.bind(this);
+    this.isValidated = this.isValidated.bind(this);
   }
-  onChange(data) {
+  onChange(data, cb) {
     console.log('App.onChange', data);
     this.setState({ ...data }, function() {
       console.log('App.onChange.then', this.state);
+      if (cb) {
+        cb();
+      }
     });
   }
   nextStep() {
-    this.setState({ step: ++this.state.step });
+    // check that form is valid for given step and throw errors on any invalid fields
+    this.isValidated().then(function () {
+      this.setState({ step: ++this.state.step, validation: { valid: false } });
+    }.bind(this));
+  }
+  isValidated() {
+    return new Promise(function (resolve, reject) {
+      let validation = {...this.state.validation};
+      let invalidFields = Object.keys(validation.fields).filter(k => !validation.fields[k].valid);
+      // set valid based on remaining invalid fields
+      validation.valid = invalidFields.length === 0;
+      // set invalid fields dirty state to true to force errors to user
+      invalidFields.forEach((k) => validation.fields[k].dirty = true);
+      
+      this.setState({ validation }, () => {
+        if (!this.state.validation.valid) {
+          reject();
+        } else {
+          resolve();
+        }
+      });
+    }.bind(this));
+  }
+  onValidation(e) {
+    let validation = {...this.state.validation, e};
+    // check each field and set valid to false if there are invalid fields
+    validation.valid = Object.keys(validation.fields).filter(k => !validation.fields[k].valid).length === 0;
+    this.setState({ validation });
   }
   render() {
     return (
       <div>
         <HorizontalLinearStepper step={this.state.step} />
 
-        {this.state.step === 0 ? <TextForm onChange={this.onChange} region={this.state.region} employeeCount={this.state.employeeCount} /> : null}
+        {this.state.step === 0 ? <TextForm onChange={this.onChange} onValidation={this.onValidation} validation={this.state.validation} region={this.state.region} employeeCount={this.state.employeeCount} /> : null}
         {this.state.step === 1 ? <TextForm1 user={this.state.user} onChange={this.onChange} /> : null}
         {this.state.step === 2 ? <TextForm2 company={this.state.company} onChange={this.onChange} /> : null}
         {this.state.step === 3 ? <TextForm3 billing={this.state.billing} onChange={this.onChange} /> : null}
+        {this.state.step === 4 ? <TextForm4 billing={this.state.billing} onChange={this.onChange} /> : null}
+        {this.state.step === 5 ? <TextForm5/> : null}
+        {this.state.step === 6 ? <TextForm6/> : null}
         {/* <TextForm3a/>
         <TextForm4/>
         <TextForm4a/>
