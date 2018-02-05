7b79c4a863935f244b65155064b59c0e01b21e0f
Mohammed Qureshi <mohammed.qureshi@gmail.com>
2018-02-04 20:17:39 -0700

    - fix styling of forms - changed text for button

--- src/components/formAdmin5.js
+++ src/components/formAdmin5.js
@@ -2,17 +2,13 @@ import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import FlatButton from 'material-ui/FlatButton';
 
-const TextForm2 = () => (<div className="container">
+const TextForm2 = () => (
   <form>
     <h2>Credit card info</h2>
     <div className="formRow">
       <div className="iframe">This is the iframe for payment method</div>
     </div>
-    <div className="formButton">
-      <FlatButton label="Cancel"/>
-      <RaisedButton label="Continue" primary={true}/>
-    </div>
   </form>
-</div>);
+);
 
 export default TextForm2;
