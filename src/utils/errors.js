c64816cbfd4a61bb462647a1a6a3877a0cca3971
Mohammed Qureshi <mohammed.qureshi@gmail.com>
2018-02-15 16:07:17 -0700

    - error util that maps errors returned from firebase or other libraries/services so that they can be handled by the app.

---
+++ src/utils/errors.js
@@ -0,0 +1,37 @@
+const _errorCodes = {
+    'auth/email-already-in-use': {
+        type: 'validation',
+        source: 'Firebase',
+        field: 'email',
+        message: 'a user with this email address already exists'
+    },
+    'auth/invalid-email': {
+        type: 'validation',
+        source: 'Firebase',
+        field: 'email',
+        message: 'You must enter a valid Email address'
+    },
+    'auth/operation-not-allowed': {
+        type: 'uncaught',
+        source: 'Firebase',
+        message: 'Unable to register user'
+    },
+    'auth/weak-password': {
+        type: 'validation',
+        source: 'Firebase',
+        field: 'password',
+        message: 'Password must be at least 8 characters'
+    },
+    'uncaughtexception': {
+        type: 'uncaught',
+        message: 'An Unexpected Error has occurred'
+    }
+};
+
+const Errors = {
+    get: (code) => {
+        return _errorCodes[code] || _errorCodes['uncaughtexception'];
+    }
+};
+
+export default Errors;
