44fb23f7cf05faa51438c5c118be080b7e659e63
Mohammed Qureshi <mohammed.qureshi@gmail.com>
2018-02-19 21:37:47 -0700

    - initialize app, then render.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {BrowserRouter } from 'react-router-dom'
+import {BrowserRouter } from 'react-router-dom';
+import i18n from './utils/i18n';
 
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>, document.getElementById('root')
-);
+const Index = {
+    initialize: function () {
+        // initialize all required utils
+        let _promise = [];
+        
+        _promise.push(i18n.initialize(i18n.getDefaultLangCode()));
+
+        Promise.all(_promise)
+            .then(function() {
+                this.run();
+            }.bind(this))
+            .catch(function(err) {
+                console.error('Index.initialize.failed', err);
+            });
+    },
+    run: function () {
+        ReactDOM.render(
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>, document.getElementById('root')
+        );
+    }
+};
+
+Index.initialize();
 
 registerServiceWorker();
