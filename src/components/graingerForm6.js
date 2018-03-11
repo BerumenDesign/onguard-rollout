600ed9a3039c742b47e4f1d1a1286cb488886f36
Luis Berumen <BerumenDesign@users.noreply.github.com>
2018-03-11 17:10:59 -0600

    Remove divider

--- src/components/graingerForm6.js
+++ src/components/graingerForm6.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
-import Divider from 'material-ui/Divider';
 import Paper from 'material-ui/Paper';
 
 const graingerForm6 = () => (<div class="container">
@@ -9,7 +8,7 @@ const graingerForm6 = () => (<div class="container">
     <h2>You are good to go!</h2>
 
     <h3>Do you want invite  more ERC users?</h3>
-    <RaisedButton label="Invite ERC users" primary={true}/>
+    <RaisedButton label="Invite ERC users" secondary={false}/>
     <FlatButton label="OR DO IT LATER IN USER ADMIN TOOL" primary={false}/>
 
     <Paper zDepth={1} className="section">
@@ -17,6 +16,9 @@ const graingerForm6 = () => (<div class="container">
 
     <FlatButton label="(Note: Add URL)" primary={false}/>
   </Paper>
+  </div>
+  <div className="formButton">
+    <RaisedButton label="Continue" primary={true} fullWidth={true}/>
   </div>
 </div>);
 
