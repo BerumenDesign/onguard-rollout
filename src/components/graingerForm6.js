3ad8b8887f5f6a68cab8a7515b75014110f7234f
Luis Berumen <BerumenDesign@users.noreply.github.com>
2018-03-11 17:00:11 -0600

    Add section for inviting more users

--- src/components/graingerForm6.js
+++ src/components/graingerForm6.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import Divider from 'material-ui/Divider';
+import Paper from 'material-ui/Paper';
 
 const graingerForm6 = () => (<div class="container">
   <div className="formRowCenter">
     <h2>You are good to go!</h2>
+
+    <h3>Do you want invite  more ERC users?</h3>
+    <RaisedButton label="Invite ERC users" primary={true}/>
+    <FlatButton label="OR DO IT LATER IN USER ADMIN TOOL" primary={false}/>
+
+    <Paper zDepth={1} className="section">
     <p>To use the web ERC, users and admins can use this link</p>
 
     <FlatButton label="(Note: Add URL)" primary={false}/>
+  </Paper>
   </div>
 </div>);
 
