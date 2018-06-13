614811ffa37c8441e1fba182486393e2053bb26e
Mohammed Qureshi <mohammed.qureshi@gmail.com>
2018-06-13 08:04:09 -0600

    feat: attach continue to button

--- src/components/formAdmin6.js
+++ src/components/formAdmin6.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import RaisedButton from 'material-ui/RaisedButton';
 import FlatButton from 'material-ui/FlatButton';
+import PropTypes from 'prop-types'
 
-const TextForm6 = () => (
+const TextForm6 = (props) => (
   <div className="formRowCenter">
     <h2>Congratulations</h2>
     <p>Your admin account has been created</p>
 
-    <h3>Do you want to create a group and invite users?</h3>
-    <RaisedButton label="YES, LET'S DO IT NOW" primary={true}/> <br />
+    <h3>Do you want to invite users?</h3>
+    <RaisedButton label="YES, LET'S DO IT NOW" primary={true} onClick={props.onContinue}/> <br />
     <FlatButton label="OR DO IT LATER IN GROUP SETTINGS" primary={false}/>
   </div>
 );
+
+TextForm6.propTypes = {
+    onContinue: PropTypes.func.isRequired
+};
 
 export default TextForm6;
