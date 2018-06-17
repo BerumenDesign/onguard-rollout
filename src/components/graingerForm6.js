7e86b5cecf292c205434dec4827be67bb15576fd
Mohammed Qureshi <mohammed.qureshi@gmail.com>
2018-06-17 16:16:39 -0600

    feat: handle next for new invite

--- src/components/graingerForm6.js
+++ src/components/graingerForm6.js
@@ -3,23 +3,28 @@ import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import Paper from 'material-ui/Paper';
 
-const graingerForm6 = () => (<div class="container">
-  <div className="formRowCenter">
-    <h2>You are good to go!</h2>
+const graingerForm6 = (props) => {
+  const newInvite = () => {
+    props.onContinue('invite');
+  };
 
-    <h3>Do you want invite more ERC users?</h3>
-    <RaisedButton label="Invite ERC users" secondary={false}/>
-    <FlatButton label="OR DO IT LATER IN USER ADMIN TOOL" primary={false}/>
+  return (
+    <div className="container">
+      <div className="formRowCenter">
+        <h2>You are good to go!</h2>
 
-    <Paper zDepth={1} className="section">
-    <p>To use the web ERC, users and admins can use this link</p>
+        <h3>Do you want invite more ERC users?</h3>
+        <RaisedButton label="Invite ERC users" onClick={newInvite} secondary={false}/>
+        <FlatButton label="OR DO IT LATER IN USER ADMIN TOOL" primary={false}/>
 
-    <FlatButton label="(Note: Add URL)" primary={false}/>
-  </Paper>
-  </div>
-  <div className="formButton">
-    <RaisedButton label="Continue" primary={true} fullWidth={true}/>
-  </div>
-</div>);
+        <Paper zDepth={1} className="section">
+          <p>To use the web ERC, users and admins can use this link</p>
+
+          <FlatButton label="(Note: Add URL)" primary={false}/>
+        </Paper>
+      </div>
+    </div>
+  );
+}
 
 export default graingerForm6;
